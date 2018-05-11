5b66bb66aa6943e97f90763e4b23f6866747d59b
Hanno <hanno@fleetconnect.co.za>
2018-05-11 21:21:49 +0200

    grt

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace WebApplication1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                    name: "Default",
+                    name: "Index",
                     url: "{controller}/{action}/{id}",
                     defaults: new { action = "Index", id = UrlParameter.Optional }
                 );
